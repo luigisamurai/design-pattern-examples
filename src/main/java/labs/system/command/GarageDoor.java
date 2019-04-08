7d08004edfe76a740ec213e65330ac38daa5a260
Luis Alberto Bolaños <labs.sistemas@gmail.com>
2019-04-08 11:56:04 -0500

    Command (#3)
    * Command pattern

---
+++ src/main/java/labs/system/command/GarageDoor.java
@@ -0,0 +1,12 @@
+package labs.system.command;
+
+public class GarageDoor {
+    public void open() {
+        System.out.println("GarageDoor.open");
+    }
+
+    public void close() {
+        System.out.println("GarageDoor.close");
+    }
+}
+
---
+++ src/main/java/labs/system/command/GarageDoorOpenCommand.java
@@ -0,0 +1,13 @@
+package labs.system.command;
+
+public class GarageDoorOpenCommand implements Command {
+    GarageDoor garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    public void execute() {
+        garageDoor.open();
+    }
+}
